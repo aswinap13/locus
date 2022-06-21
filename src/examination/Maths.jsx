e32f46767e7ef50174835e75ee4f1c8b9d1f6e11
Aswin <aswinapxyz@cet.ac.in>
2022-06-21 21:30:15 +0530

    Exam portal without Timer

---
+++ src/examination/Maths.jsx
@@ -0,0 +1,147 @@
+import React,{useState} from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import classes from  './examination.module.css';
+
+ const Maths=() => {
+  
+  return ( 
+        <div className={`${classes.Maths} row`}>
+               
+               <button className={`${classes.questionChose} col-md-1`}>1</button>
+                <button className={`${classes.questionChose} col-md-1`}>2</button>
+                <button className={`${classes.questionChose} col-md-1`}>3</button>
+                <button className={`${classes.questionChose} col-md-1`}>4</button>
+                <button className={`${classes.questionChose} col-md-1`}>5</button>
+                <button className={`${classes.questionChose} col-md-1`}>6</button>
+                <button className={`${classes.questionChose} col-md-1`}>7</button>
+                <button className={`${classes.questionChose} col-md-1`}>8</button>
+                <button className={`${classes.questionChose} col-md-1`}>9</button>
+                <button className={`${classes.questionChose} col-md-1`}>10</button>
+                <button className={`${classes.questionChose} col-md-1`}>11</button>
+                <button className={`${classes.questionChose} col-md-1`}>12</button>
+                <button className={`${classes.questionChose} col-md-1`}>13</button>
+                <button className={`${classes.questionChose} col-md-1`}>14</button>
+                <button className={`${classes.questionChose} col-md-1`}>15</button>
+                <button className={`${classes.questionChose} col-md-1`}>16</button>
+                <button className={`${classes.questionChose} col-md-1`}>17</button>
+                <button className={`${classes.questionChose} col-md-1`}>18</button>
+                <button className={`${classes.questionChose} col-md-1`}>19</button>
+                <button className={`${classes.questionChose} col-md-1`}>20</button>
+                <button className={`${classes.questionChose} col-md-1`}>21</button>
+                <button className={`${classes.questionChose} col-md-1`}>22</button>
+                <button className={`${classes.questionChose} col-md-1`}>23</button>
+                <button className={`${classes.questionChose} col-md-1`}>24</button>
+                <button className={`${classes.questionChose} col-md-1`}>25</button>
+                <button className={`${classes.questionChose} col-md-1`}>26</button>
+                <button className={`${classes.questionChose} col-md-1`}>27</button>
+                <button className={`${classes.questionChose} col-md-1`}>28</button>
+                <button className={`${classes.questionChose} col-md-1`}>29</button>
+                <button className={`${classes.questionChose} col-md-1`}>30</button>
+                <button className={`${classes.questionChose} col-md-1`}>31</button>
+                <button className={`${classes.questionChose} col-md-1`}>32</button>
+                <button className={`${classes.questionChose} col-md-1`}>33</button>
+                <button className={`${classes.questionChose} col-md-1`}>34</button>
+                <button className={`${classes.questionChose} col-md-1`}>35</button>
+                <button className={`${classes.questionChose} col-md-1`}>36</button>
+                <button className={`${classes.questionChose} col-md-1`}>37</button>
+                <button className={`${classes.questionChose} col-md-1`}>38</button>
+                <button className={`${classes.questionChose} col-md-1`}>39</button>
+                <button className={`${classes.questionChose} col-md-1`}>40</button>
+                <button className={`${classes.questionChose} col-md-1`}>41</button>
+                <button className={`${classes.questionChose} col-md-1`}>42</button>
+                <button className={`${classes.questionChose} col-md-1`}>43</button>
+                <button className={`${classes.questionChose} col-md-1`}>44</button>
+                <button className={`${classes.questionChose} col-md-1`}>45</button>
+                <button className={`${classes.questionChose} col-md-1`}>46</button>
+                <button className={`${classes.questionChose} col-md-1`}>47</button>
+                <button className={`${classes.questionChose} col-md-1`}>48</button>
+                <button className={`${classes.questionChose} col-md-1`}>49</button>
+                <button className={`${classes.questionChose} col-md-1`}>50</button>
+                <button className={`${classes.questionChose} col-md-1`}>51</button>
+                <button className={`${classes.questionChose} col-md-1`}>52</button>
+                <button className={`${classes.questionChose} col-md-1`}>53</button>
+                <button className={`${classes.questionChose} col-md-1`}>54</button>
+                <button className={`${classes.questionChose} col-md-1`}>55</button>
+                <button className={`${classes.questionChose} col-md-1`}>56</button>
+                <button className={`${classes.questionChose} col-md-1`}>57</button>
+                <button className={`${classes.questionChose} col-md-1`}>58</button>
+                <button className={`${classes.questionChose} col-md-1`}>59</button>
+                <button className={`${classes.questionChose} col-md-1`}>60</button>
+                <button className={`${classes.questionChose} col-md-1`}>61</button>
+                <button className={`${classes.questionChose} col-md-1`}>62</button>
+                <button className={`${classes.questionChose} col-md-1`}>63</button>
+                <button className={`${classes.questionChose} col-md-1`}>64</button>
+                <button className={`${classes.questionChose} col-md-1`}>65</button>
+                <button className={`${classes.questionChose} col-md-1`}>66</button>
+                <button className={`${classes.questionChose} col-md-1`}>67</button>
+                <button className={`${classes.questionChose} col-md-1`}>68</button>
+                <button className={`${classes.questionChose} col-md-1`}>69</button>
+                <button className={`${classes.questionChose} col-md-1`}>70</button>
+                <button className={`${classes.questionChose} col-md-1`}>71</button>
+                <button className={`${classes.questionChose} col-md-1`}>72</button>
+                <button className={`${classes.questionChose} col-md-1`}>73</button>
+                <button className={`${classes.questionChose} col-md-1`}>74</button>
+                <button className={`${classes.questionChose} col-md-1`}>75</button>
+                <button className={`${classes.questionChose} col-md-1`}>76</button>
+                <button className={`${classes.questionChose} col-md-1`}>77</button>
+                <button className={`${classes.questionChose} col-md-1`}>78</button>
+                <button className={`${classes.questionChose} col-md-1`}>79</button>
+                <button className={`${classes.questionChose} col-md-1`}>80</button>
+                <button className={`${classes.questionChose} col-md-1`}>81</button>
+                <button className={`${classes.questionChose} col-md-1`}>82</button>
+                <button className={`${classes.questionChose} col-md-1`}>83</button>
+                <button className={`${classes.questionChose} col-md-1`}>84</button>
+                <button className={`${classes.questionChose} col-md-1`}>85</button>
+                <button className={`${classes.questionChose} col-md-1`}>86</button>
+                <button className={`${classes.questionChose} col-md-1`}>87</button>
+                <button className={`${classes.questionChose} col-md-1`}>88</button>
+                <button className={`${classes.questionChose} col-md-1`}>89</button>
+                <button className={`${classes.questionChose} col-md-1`}>90</button>
+                <button className={`${classes.questionChose} col-md-1`}>91</button>
+                <button className={`${classes.questionChose} col-md-1`}>92</button>
+                <button className={`${classes.questionChose} col-md-1`}>93</button>
+                <button className={`${classes.questionChose} col-md-1`}>94</button>
+                <button className={`${classes.questionChose} col-md-1`}>95</button>
+                <button className={`${classes.questionChose} col-md-1`}>96</button>
+                <button className={`${classes.questionChose} col-md-1`}>97</button>
+                <button className={`${classes.questionChose} col-md-1`}>98</button>
+                <button className={`${classes.questionChose} col-md-1`}>99</button>
+                <button className={`${classes.questionChose} col-md-1`}>100</button>
+                <button className={`${classes.questionChose} col-md-1`}>101</button>
+                <button className={`${classes.questionChose} col-md-1`}>102</button>
+                <button className={`${classes.questionChose} col-md-1`}>103</button>
+                <button className={`${classes.questionChose} col-md-1`}>104</button>
+                <button className={`${classes.questionChose} col-md-1`}>105</button>
+                <button className={`${classes.questionChose} col-md-1`}>106</button>
+                <button className={`${classes.questionChose} col-md-1`}>107</button>
+                <button className={`${classes.questionChose} col-md-1`}>108</button>
+                <button className={`${classes.questionChose} col-md-1`}>109</button>
+                <button className={`${classes.questionChose} col-md-1`}>110</button>
+                <button className={`${classes.questionChose} col-md-1`}>111</button>
+                <button className={`${classes.questionChose} col-md-1`}>112</button>
+                <button className={`${classes.questionChose} col-md-1`}>113</button>
+                <button className={`${classes.questionChose} col-md-1`}>114</button>
+                <button className={`${classes.questionChose} col-md-1`}>115</button>
+                <button className={`${classes.questionChose} col-md-1`}>116</button>
+                <button className={`${classes.questionChose} col-md-1`}>117</button>
+                <button className={`${classes.questionChose} col-md-1`}>118</button>
+                <button className={`${classes.questionChose} col-md-1`}>119</button>
+                <button className={`${classes.questionChose} col-md-1`}>120</button>
+        </div>
+
+
+    )
+        }
+export default Maths;
+
+
+
+
+
+
+
+
+
+
+
+
