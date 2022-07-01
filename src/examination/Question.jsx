50317c50100f782a0704d43922f4eba3a438a409
fausan123 <fausan.asharaf01@gmail.com>
2022-07-02 00:22:26 +0530

    added latex rep

--- src/examination/Question.jsx
+++ src/examination/Question.jsx
@@ -1,8 +1,10 @@
 import React,{useState} from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import classes from  './examination.module.css';
+var Latex = require('react-latex');
 
  const Question=({ question, currentoption, setCurrentoption}) => {
+    const fraction = `What does $$x=\\frac{-b+\\sqrt{b^2-4ac}}{2a}$$ give?`
 
     const handleOptionChange = (e) => {
         setCurrentoption(e.target.value);
@@ -11,24 +13,28 @@ import classes from  './examination.module.css';
   return ( 
         <div className={classes.question}>
             <div className={classes.currentQues}>
-            { question && <h3>{question.question_no}.  {question.title}</h3>}
+            { question && <h4>{question.question_no}. <Latex>{question.title}</Latex></h4>}
             
             <div className={classes.options} onChange={handleOptionChange}>
                 <div className={classes.option}>
                     <input type='radio' id="optionA" name="options" value="A" checked={currentoption === "A"}/>
-                    <label for="lname">A : {question.option_a}</label>
+                    <label for="lname">A : <Latex>{question.option_a}</Latex></label>
                 </div>
                <div className={classes.option}>
                 <input type='radio' id="optionB" name="options" value="B" checked={currentoption === "B"}/>
-                    <label for="lname">B : {question.option_b}</label>
+                    <label for="lname">B : <Latex>{question.option_b}</Latex></label>
                </div>
                 <div className={classes.option}>
                     <input type='radio' id="optionC" name="options" value="C" checked={currentoption === "C"}/>
-                    <label for="lname">C : {question.option_c}</label>
+                    <label for="lname">C : <Latex>{question.option_c}</Latex></label>
                 </div>
                <div className={classes.option}>
                <input type='radio' id="optionD" name="options" value="D" checked={currentoption === "D"}/>
-                <label for="lname">D : {question.option_d}</label>
+                <label for="lname">D : <Latex>{question.option_d}</Latex></label>
+               </div>
+               <div className={classes.option}>
+               <input type='radio' id="optionE" name="options" value="E" checked={currentoption === "E"}/>
+                <label for="lname">E : <Latex>{question.option_e}</Latex></label>
                </div>
             </div>
             </div>
